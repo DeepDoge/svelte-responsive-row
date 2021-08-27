958b17b26fb8d521d37315d4fd6d0a733ab4161f
Shiba <44804845+DeepDoge@users.noreply.github.com>
2021-08-27 07:17:35 +0000

    🍙

---
+++ Row.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+    export let idealSize: string = "0px";
+    export let gap: string = '.1em';
+</script>
+
+<div style="--ideal-size:{idealSize};--gap:{gap}">
+    <slot />
+</div>
+
+<style>
+    div {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(min(var(--ideal-size), 100%), 1fr));
+        gap: var(--gap);
+    }
+</style>
