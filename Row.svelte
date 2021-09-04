2ce40f144ca387c405553312157a0348ae899e51
Shiba <44804845+DeepDoge@users.noreply.github.com>
2021-09-04 08:09:17 +0300

    🍙 this is a better default

--- Row.svelte
+++ Row.svelte
@@ -1,22 +1,18 @@
 <script lang="ts">
+    export let type: 'fit' | 'fill' = 'fill'
     export let idealSize: string = "0px";
-    export let maxWidth: string = "100%";
     export let gap: string = ".1em";
 </script>
 
-<div style="--ideal-size:{idealSize};--gap:{gap};--max-width:{maxWidth}">
+<div style="--ideal-size:{idealSize};--gap:{gap};--type:auto-{type}">
     <slot />
 </div>
 
 <style>
     div {
         display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(min(var(--ideal-size), 100%), 1fr));
+        grid-template-columns: repeat(var(--type), minmax(min(var(--ideal-size), 100%), 1fr));
         gap: var(--gap);
         justify-items: center;
-    }
-
-    div > :global(*) {
-        max-width: var(--max-width);
     }
 </style>
