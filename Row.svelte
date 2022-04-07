af8670aa3017c9b3b177fd2cbe0bd9d70727c402
Shiba <44804845+DeepDoge@users.noreply.github.com>
2022-04-07 15:48:53 +0000

    🍣max column count added

--- Row.svelte
+++ Row.svelte
@@ -2,17 +2,19 @@
     export let type: "fit" | "fill" = "fill";
     export let idealSize: string = "0px";
     export let gap: string = ".1em";
+    export let maxColumnCount: number = Number.MAX_SAFE_INTEGER;
+    export let justifyItems: 'center' | 'start' | 'end' |'stretch' = 'center'
 </script>
 
-<div style="--ideal-size:{idealSize};--gap:{gap};--type:auto-{type}">
+<div style="--ideal-size:{idealSize};--gap:{gap};--type:auto-{type};--max-column-count:{maxColumnCount};--justify-items:{justifyItems}">
     <slot />
 </div>
 
 <style>
     div {
         display: grid;
-        grid-template-columns: repeat(var(--type), minmax(min(var(--ideal-size), 100%), 1fr));
+        grid-template-columns: repeat(auto-fit, minmax(min(max(100% / var(--max-column-count) - var(--gap) / 1.5, var(--ideal-size)), 100%), 1fr));
         gap: var(--gap);
-        justify-items: center;
+        justify-items: var(--justify-items);
     }
 </style>
