cd41f9ce6d89f86634e8a71b84909a59025445f1
Shiba <44804845+DeepDoge@users.noreply.github.com>
2022-04-07 16:07:03 +0000

    🍣 max column count with gap bug fix

--- Row.svelte
+++ Row.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
     export let type: "fit" | "fill" = "fill";
     export let idealSize: string = "0px";
-    export let gap: string = ".1em";
+    export let gap: string = ".5em";
     export let maxColumnCount: number = Number.MAX_SAFE_INTEGER;
-    export let justifyItems: 'center' | 'start' | 'end' |'stretch' = 'center'
+    export let justifyItems: "center" | "start" | "end" | "stretch" = "center";
 </script>
 
 <div style="--ideal-size:{idealSize};--gap:{gap};--type:auto-{type};--max-column-count:{maxColumnCount};--justify-items:{justifyItems}">
@@ -13,7 +13,7 @@
 <style>
     div {
         display: grid;
-        grid-template-columns: repeat(auto-fit, minmax(min(max(100% / var(--max-column-count) - var(--gap) / 1.5, var(--ideal-size)), 100%), 1fr));
+        grid-template-columns: repeat(var(--type), minmax(min(max(100% / var(--max-column-count) - var(--gap), var(--ideal-size)), 100%), 1fr));
         gap: var(--gap);
         justify-items: var(--justify-items);
     }
